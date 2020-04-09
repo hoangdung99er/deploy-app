a51979c13ae4d0a72c377e204dd67679da508fcf
hoangdung99er <hoangdung99er@gmail.com>
2020-04-09 22:52:07 +0700

    Updates

---
+++ precache-manifest.baec4d72e6d9bea00955e55e868bc177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ec17f63a2d80ec9745dbb48e524b67",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "4ea0fa2c3b62d45a9ad6",
+    "url": "/deploy-app/static/css/main.9e49bd3d.chunk.css"
+  },
+  {
+    "revision": "2cc83b0e1e36122d0759",
+    "url": "/deploy-app/static/js/2.d7bfa565.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/deploy-app/static/js/2.d7bfa565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea0fa2c3b62d45a9ad6",
+    "url": "/deploy-app/static/js/main.2bbfe503.chunk.js"
+  },
+  {
+    "revision": "d58c2e0723953ba89dd6",
+    "url": "/deploy-app/static/js/runtime-main.83601c3b.js"
+  }
+]);
